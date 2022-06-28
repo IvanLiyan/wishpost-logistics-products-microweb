7ef1d440476d40d9036119099ef1653dff295fd0
Kay <kwan@wish.com>
2022-06-28 14:21:34 +0800

    fix:bug

--- src/views/manage-order-traffic-control/url.js
+++ src/views/manage-order-traffic-control/url.js
@@ -1,4 +1,4 @@
 export default {
   getOrderTrafficControlList: '/api/order-traffic-control-config/order_traffic_control/list',
-  getOrderTrafficControlMeta: 'order-traffic-control-config/order_traffic_control/meta',
+  getOrderTrafficControlMeta: '/api/order-traffic-control-config/order_traffic_control/meta',
 };
