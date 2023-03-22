bc6e6388c9b95157216fe4b31c4e4c65ab9b9ba8
Rose <ryu@wish.com>
2023-03-22 16:36:37 +0700

    delete use plugin

--- vue.config.js
+++ vue.config.js
@@ -71,21 +71,6 @@ module.exports = {
       ]);
   },
   configureWebpack: {
-    // module: {
-    //   rules: [
-    //     {
-    //       test: /\.(react)\.?(jsx)(\?.*)?$/,
-    //       exclude: /(node_modules|bower_components)/,
-    //       use: {
-    //         loader: 'babel-loader',
-    //         options: {
-    //           presets: ['@babel/preset-react'],
-    //           plugins: ['transform-react-jsx'],
-    //         },
-    //       },
-    //     },
-    //   ],
-    // },
     // 支持jquery
     plugins: [
       new webpack.ProvidePlugin({
